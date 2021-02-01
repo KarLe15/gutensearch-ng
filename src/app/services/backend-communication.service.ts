052ad2488b6ea9f96ecdda24a418c83dbdcf89c9
Karim Leffad <leffadkarim97@live.fr>
2021-02-01 10:24:34 +0100

    Avant

--- src/app/services/backend-communication.service.ts
+++ src/app/services/backend-communication.service.ts
@@ -23,259 +23,262 @@ export class BackendCommunicationService {
     //     body: req,
     //   }
     // );
-    const res: SearchResponse = {
-      searchResponses : [
-        {
-          hash: 'kkksqslmjfn#', // ne pas utiliser
-          book: {
-            id: 1342,
-            title: 'Pride and Prejudice',
-            authors: [
-              {
-                last_name: 'Austen',
-                first_name: 'Jane',
-                authorBirth: '1775',
-                authorDeath: '1817'
-              }
-            ],
-            subjects: [
-              'Courtship -- Fiction',
-              'Domestic fiction',
-              'England -- Fiction',
-              'Love stories',
-              'Sisters -- Fiction',
-              'Social classes -- Fiction',
-              'Young women -- Fiction'
-            ],
-            bookShelves: [
-              'Best Books Ever Listings',
-              'Harvard Classics'
-            ],
-            copyright: false,
-            mediaType: 'Text',
-            downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
-            downloadCount: 57065
-          },
-          words: [
-            {
-              word: 'with',
-              occurrence: 1024
-            },
-            {
-              word: 'without',
-              occurrence: 86
-            }
-          ],
-          accuracy: 0.64
-        },
-        {
-          hash: 'kkksqslmjfn#', // ne pas utiliser
-          book: {
-            id: 1342,
-            title: 'Pride and Prejudice',
-            authors: [
-              {
-                last_name: 'Austen',
-                first_name: 'Jane',
-                authorBirth: '1775',
-                authorDeath: '1817'
-              }
-            ],
-            subjects: [
-              'Courtship -- Fiction',
-              'Domestic fiction',
-              'England -- Fiction',
-              'Love stories',
-              'Sisters -- Fiction',
-              'Social classes -- Fiction',
-              'Young women -- Fiction'
-            ],
-            bookShelves: [
-              'Best Books Ever Listings',
-              'Harvard Classics'
-            ],
-            copyright: false,
-            mediaType: 'Text',
-            downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
-            downloadCount: 57065
-          },
-          words: [
-            {
-              word: 'with',
-              occurrence: 1024
-            },
-            {
-              word: 'without',
-              occurrence: 86
-            }
-          ],
-          accuracy: 0.64
-        }
-      ],
-      suggestions: [
-
-        {
-          id: 1342,
-          title: 'Pride and Prejudice',
-          authors: [
-            {
-              last_name: 'Austen',
-              first_name: 'Jane',
-              authorBirth: '1775',
-              authorDeath: '1817'
-            }
-          ],
-          subjects: [
-            'Courtship -- Fiction',
-            'Domestic fiction',
-            'England -- Fiction',
-            'Love stories',
-            'Sisters -- Fiction',
-            'Social classes -- Fiction',
-            'Young women -- Fiction'
-          ],
-          bookShelves: [
-            'Best Books Ever Listings',
-            'Harvard Classics'
-          ],
-          copyright: false,
-          mediaType: 'Text',
-          downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
-          downloadCount: 57065
-        },
-
-      ]
-
-    };
-    return of(res);
+    return this.http.get<SearchResponse>(
+      `${this.baseURL}/v1/api/search?isRegex=${req.isRegex}&query=${encodeURI(req.query)}`
+    );
+    // const res: SearchResponse = {
+    //   searchResponses : [
+    //     {
+    //       hash: 'kkksqslmjfn#', // ne pas utiliser
+    //       book: {
+    //         id: 1342,
+    //         title: 'Pride and Prejudice',
+    //         authors: [
+    //           {
+    //             last_name: 'Austen',
+    //             first_name: 'Jane',
+    //             authorBirth: '1775',
+    //             authorDeath: '1817'
+    //           }
+    //         ],
+    //         subjects: [
+    //           'Courtship -- Fiction',
+    //           'Domestic fiction',
+    //           'England -- Fiction',
+    //           'Love stories',
+    //           'Sisters -- Fiction',
+    //           'Social classes -- Fiction',
+    //           'Young women -- Fiction'
+    //         ],
+    //         bookShelves: [
+    //           'Best Books Ever Listings',
+    //           'Harvard Classics'
+    //         ],
+    //         copyright: false,
+    //         mediaType: 'Text',
+    //         downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
+    //         downloadCount: 57065
+    //       },
+    //       words: [
+    //         {
+    //           word: 'with',
+    //           occurrence: 1024
+    //         },
+    //         {
+    //           word: 'without',
+    //           occurrence: 86
+    //         }
+    //       ],
+    //       accuracy: 0.64
+    //     },
+    //     {
+    //       hash: 'kkksqslmjfn#', // ne pas utiliser
+    //       book: {
+    //         id: 1342,
+    //         title: 'Pride and Prejudice',
+    //         authors: [
+    //           {
+    //             last_name: 'Austen',
+    //             first_name: 'Jane',
+    //             authorBirth: '1775',
+    //             authorDeath: '1817'
+    //           }
+    //         ],
+    //         subjects: [
+    //           'Courtship -- Fiction',
+    //           'Domestic fiction',
+    //           'England -- Fiction',
+    //           'Love stories',
+    //           'Sisters -- Fiction',
+    //           'Social classes -- Fiction',
+    //           'Young women -- Fiction'
+    //         ],
+    //         bookShelves: [
+    //           'Best Books Ever Listings',
+    //           'Harvard Classics'
+    //         ],
+    //         copyright: false,
+    //         mediaType: 'Text',
+    //         downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
+    //         downloadCount: 57065
+    //       },
+    //       words: [
+    //         {
+    //           word: 'with',
+    //           occurrence: 1024
+    //         },
+    //         {
+    //           word: 'without',
+    //           occurrence: 86
+    //         }
+    //       ],
+    //       accuracy: 0.64
+    //     }
+    //   ],
+    //   suggestions: [
+    //
+    //     {
+    //       id: 1342,
+    //       title: 'Pride and Prejudice',
+    //       authors: [
+    //         {
+    //           last_name: 'Austen',
+    //           first_name: 'Jane',
+    //           authorBirth: '1775',
+    //           authorDeath: '1817'
+    //         }
+    //       ],
+    //       subjects: [
+    //         'Courtship -- Fiction',
+    //         'Domestic fiction',
+    //         'England -- Fiction',
+    //         'Love stories',
+    //         'Sisters -- Fiction',
+    //         'Social classes -- Fiction',
+    //         'Young women -- Fiction'
+    //       ],
+    //       bookShelves: [
+    //         'Best Books Ever Listings',
+    //         'Harvard Classics'
+    //       ],
+    //       copyright: false,
+    //       mediaType: 'Text',
+    //       downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
+    //       downloadCount: 57065
+    //     },
+    //
+    //   ]
+    //
+    // };
+    // return of(res);
   }
 
   public getBookDetails(req: BookRequest): Observable<BookResponse> {
-    // return this.http.request<BookResponse>(
-    //   'GET', `${this.baseURL}/v1/api/book`,
-    //   {
-    //     body: req
-    //   }
-    // );
-    const res: BookResponse = {
-      book:  {
-        id: 1342,
-        title: 'Pride and Prejudice',
-        authors: [
-          {
-            last_name: 'Austen',
-            first_name: 'Jane',
-            authorBirth: '1775',
-            authorDeath: '1817'
-          }
-        ],
-        subjects: [
-          'Courtship -- Fiction',
-          'Domestic fiction',
-          'England -- Fiction',
-          'Love stories',
-          'Sisters -- Fiction',
-          'Social classes -- Fiction',
-          'Young women -- Fiction'
-        ],
-        bookShelves: [
-          'Best Books Ever Listings',
-          'Harvard Classics'
-        ],
-        copyright: false,
-        mediaType: 'Text',
-        downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
-        downloadCount: 57065
-      },
-      suggestions: [
-        {
-          id: 1342,
-          title: 'Pride and Prejudice',
-          authors: [
-            {
-              last_name: 'Austen',
-              first_name: 'Jane',
-              authorBirth: '1775',
-              authorDeath: '1817'
-            }
-          ],
-          subjects: [
-            'Courtship -- Fiction',
-            'Domestic fiction',
-            'England -- Fiction',
-            'Love stories',
-            'Sisters -- Fiction',
-            'Social classes -- Fiction',
-            'Young women -- Fiction'
-          ],
-          bookShelves: [
-            'Best Books Ever Listings',
-            'Harvard Classics'
-          ],
-          copyright: false,
-          mediaType: 'Text',
-          downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
-          downloadCount: 57065
-        },{
-          id: 1342,
-          title: 'Pride and Prejudice',
-          authors: [
-            {
-              last_name: 'Austen',
-              first_name: 'Jane',
-              authorBirth: '1775',
-              authorDeath: '1817'
-            }
-          ],
-          subjects: [
-            'Courtship -- Fiction',
-            'Domestic fiction',
-            'England -- Fiction',
-            'Love stories',
-            'Sisters -- Fiction',
-            'Social classes -- Fiction',
-            'Young women -- Fiction'
-          ],
-          bookShelves: [
-            'Best Books Ever Listings',
-            'Harvard Classics'
-          ],
-          copyright: false,
-          mediaType: 'Text',
-          downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
-          downloadCount: 57065
-        },{
-          id: 1342,
-          title: 'Pride and Prejudice',
-          authors: [
-            {
-              last_name: 'Austen',
-              first_name: 'Jane',
-              authorBirth: '1775',
-              authorDeath: '1817'
-            }
-          ],
-          subjects: [
-            'Courtship -- Fiction',
-            'Domestic fiction',
-            'England -- Fiction',
-            'Love stories',
-            'Sisters -- Fiction',
-            'Social classes -- Fiction',
-            'Young women -- Fiction'
-          ],
-          bookShelves: [
-            'Best Books Ever Listings',
-            'Harvard Classics'
-          ],
-          copyright: false,
-          mediaType: 'Text',
-          downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
-          downloadCount: 57065
-        },
-
-      ]
-    };
-    return of(res);
+    return this.http.request<BookResponse>(
+      'GET', `${this.baseURL}/v1/api/book`,
+      {
+        body: req
+      }
+    );
+    // const res: BookResponse = {
+    //   book:  {
+    //     id: 1342,
+    //     title: 'Pride and Prejudice',
+    //     authors: [
+    //       {
+    //         last_name: 'Austen',
+    //         first_name: 'Jane',
+    //         authorBirth: '1775',
+    //         authorDeath: '1817'
+    //       }
+    //     ],
+    //     subjects: [
+    //       'Courtship -- Fiction',
+    //       'Domestic fiction',
+    //       'England -- Fiction',
+    //       'Love stories',
+    //       'Sisters -- Fiction',
+    //       'Social classes -- Fiction',
+    //       'Young women -- Fiction'
+    //     ],
+    //     bookShelves: [
+    //       'Best Books Ever Listings',
+    //       'Harvard Classics'
+    //     ],
+    //     copyright: false,
+    //     mediaType: 'Text',
+    //     downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
+    //     downloadCount: 57065
+    //   },
+    //   suggestions: [
+    //     {
+    //       id: 1342,
+    //       title: 'Pride and Prejudice',
+    //       authors: [
+    //         {
+    //           last_name: 'Austen',
+    //           first_name: 'Jane',
+    //           authorBirth: '1775',
+    //           authorDeath: '1817'
+    //         }
+    //       ],
+    //       subjects: [
+    //         'Courtship -- Fiction',
+    //         'Domestic fiction',
+    //         'England -- Fiction',
+    //         'Love stories',
+    //         'Sisters -- Fiction',
+    //         'Social classes -- Fiction',
+    //         'Young women -- Fiction'
+    //       ],
+    //       bookShelves: [
+    //         'Best Books Ever Listings',
+    //         'Harvard Classics'
+    //       ],
+    //       copyright: false,
+    //       mediaType: 'Text',
+    //       downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
+    //       downloadCount: 57065
+    //     },{
+    //       id: 1342,
+    //       title: 'Pride and Prejudice',
+    //       authors: [
+    //         {
+    //           last_name: 'Austen',
+    //           first_name: 'Jane',
+    //           authorBirth: '1775',
+    //           authorDeath: '1817'
+    //         }
+    //       ],
+    //       subjects: [
+    //         'Courtship -- Fiction',
+    //         'Domestic fiction',
+    //         'England -- Fiction',
+    //         'Love stories',
+    //         'Sisters -- Fiction',
+    //         'Social classes -- Fiction',
+    //         'Young women -- Fiction'
+    //       ],
+    //       bookShelves: [
+    //         'Best Books Ever Listings',
+    //         'Harvard Classics'
+    //       ],
+    //       copyright: false,
+    //       mediaType: 'Text',
+    //       downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
+    //       downloadCount: 57065
+    //     },{
+    //       id: 1342,
+    //       title: 'Pride and Prejudice',
+    //       authors: [
+    //         {
+    //           last_name: 'Austen',
+    //           first_name: 'Jane',
+    //           authorBirth: '1775',
+    //           authorDeath: '1817'
+    //         }
+    //       ],
+    //       subjects: [
+    //         'Courtship -- Fiction',
+    //         'Domestic fiction',
+    //         'England -- Fiction',
+    //         'Love stories',
+    //         'Sisters -- Fiction',
+    //         'Social classes -- Fiction',
+    //         'Young women -- Fiction'
+    //       ],
+    //       bookShelves: [
+    //         'Best Books Ever Listings',
+    //         'Harvard Classics'
+    //       ],
+    //       copyright: false,
+    //       mediaType: 'Text',
+    //       downloadLink: 'https://www.gutenberg.org/files/1342/1342-0.txt',
+    //       downloadCount: 57065
+    //     },
+    //
+    //   ]
+    // };
+    // return of(res);
   }
 }
